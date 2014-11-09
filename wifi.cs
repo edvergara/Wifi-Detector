24ebca9d6b72224f22c9275ec750924f901d4352
edvergara <eddianav@gmail.com>
2014-11-08 23:01:34 -0500

    Intial NativeWiFiAPI check-in plus WinForm test application.

---
+++ wifi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NativeWiFiApi
+{
+    public class wifi
+    {
+        isaNativeWiFiApi wifiAPI;
+        public wifi()
+        {
+            wifiAPI = new isaNativeWiFiApi();
+        }
+
+        public void EnumerateNICs()
+        {
+            wifiAPI.EnumerateNICs();
+        }
+    }
+}
